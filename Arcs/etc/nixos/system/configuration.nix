e16cc4baf239cd43b977c4865e614105d5f80e4c
hdengsan <sanhenden@gmail.com>
2024-09-01 20:02:13 +0300

    revert name change

--- Arcs/etc/nixos/system/configuration.nix
+++ Arcs/etc/nixos/system/configuration.nix
@@ -8,7 +8,7 @@ in
 {
   imports =
     [ # Include the results of the hardware scan...
-      ./hardware-config.nix
+      ./hardware-configuration.nix
       # Include NVIDIA configuration
       ./nvidia.nix
       # Include stylix
