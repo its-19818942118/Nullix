6ecf08c88d71f50842d0750e0d0c9038d572ff09
its-19818942118 <lordprime2020@gmail.com>
2024-09-28 05:45:00 +0600

    major code changes and refactor

--- confs/configs/bootloader.nix
+++ confs/configs/bootloader.nix
@@ -14,23 +14,23 @@
   boot.loader.systemd-boot.enable = true;
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
-  #! Enable grub below, note you will have to change to the new bios boot option for settings to apply
-  boot = {
+  # #! Enable grub below, note you will have to change to the new bios boot option for settings to apply
+  # boot = {
     
-    loader = {
+  #   loader = {
       
-      efi.canTouchEfiVariables = true;
-      grub = {
+  #     efi.canTouchEfiVariables = true;
+  #     grub = {
         
-        enable = true;
-        device = "nodev";
-        efiSupport = true;
-        useOSProber = true;
+  #       enable = true;
+  #       device = "nodev";
+  #       efiSupport = true;
+  #       useOSProber = true;
         
-      };
+  #     };
       
-    };
+  #   };
     
-  };
+  # };
   
 }
