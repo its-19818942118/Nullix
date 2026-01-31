1f2c9498f5737585eaaf1fca19ff764d1a2b10ca
Izumi <lordprime2020@gmail.com>
2026-01-31 08:55:53 -0500

    added static option and optional -s (strip) option for better binary dist

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,22 @@
 #include "lib_nullix/config.hpp"
 #include "lib_nullix/wallpaperManager.hpp"
 #include <expected>
-#include <fmt/base.h>
-#include <fmt/format.h>
+#include <print>
+// #include <fmt/base.h>
+// #include <fmt/format.h>
 using namespace std::string_view_literals;
 int main() {
     auto cfgResult = nullix::Config::load();
 
     if (!cfgResult.has_value()) {
-        fmt::println("Config error: {}", cfgResult.error().msg);
+        std::println("Config error: {}", cfgResult.error().msg);
         return 1;
     }
 
     nullix::Config& cfg = *cfgResult;
 
     nullix::WallpaperManager wm(cfg);
-    fmt::println("{}", wm.getWallpaperSettings());
+    std::println("{}", wm.getWallpaperSettings());
 
     return 0;
 }
